68984ed2d8492cae4093ce67b1147d0056a6dcb7
lorim <S1094728@studenti.univpm.t>
2025-04-03 23:22:35 +0200

    I forgot to upload this to git

--- app/src/main/java/com/example/runplusplus/view/MainActivity.kt
+++ app/src/main/java/com/example/runplusplus/view/MainActivity.kt
@@ -92,3 +92,20 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LorenzoManganelliRunTheme {
+        Greeting("Android")
+    }
+}
